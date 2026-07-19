0e14735e3c3769023e40b6b1be28348fdb6a36d4
abevol <85929368+abevol@users.noreply.github.com>
2026-07-19 08:13:10 +0800

    fix(build): restore source file and line number info in stack traces
    - Upgrade ILRepack 2.0.18 -> 2.0.46 (supports Portable PDB)
    - Switch DebugType from embedded to portable in both projects
    - Enable DebugSymbols in UniverseLib (was disabled)
    - Place PDB files alongside DLLs in all build variants

--- build.ps1
+++ build.ps1
@@ -15,8 +15,10 @@ Remove-Item $Path/Iced.dll
 Remove-Item $Path/UnhollowerBaseLib.dll
 New-Item -Path "$Path" -Name "Mods" -ItemType "directory" -Force
 Move-Item -Path $Path/CinematicUnityExplorer.ML.IL2CPP.net6preview.dll -Destination $Path/Mods -Force
+Move-Item -Path $Path/CinematicUnityExplorer.ML.IL2CPP.net6preview.pdb -Destination $Path/Mods -Force
 New-Item -Path "$Path" -Name "UserLibs" -ItemType "directory" -Force
 Move-Item -Path $Path/UniverseLib.IL2CPP.Unhollower.dll -Destination $Path/UserLibs -Force
+Copy-Item UniverseLib/Release/UniverseLib.Il2Cpp.Unhollower/UniverseLib.IL2CPP.Unhollower.pdb -Destination $Path/UserLibs -Force
 # (create zip archive)
 Remove-Item $Path/../CinematicUnityExplorer.MelonLoader.IL2CPP.net6preview.zip -ErrorAction SilentlyContinue
 compress-archive .\$Path\* $Path/../CinematicUnityExplorer.MelonLoader.IL2CPP.net6preview.zip
@@ -36,8 +38,10 @@ Remove-Item $Path/Il2CppInterop.Runtime.dll
 Remove-Item $Path/Microsoft.Extensions.Logging.Abstractions.dll
 New-Item -Path "$Path" -Name "Mods" -ItemType "directory" -Force
 Move-Item -Path $Path/CinematicUnityExplorer.ML.IL2CPP.CoreCLR.dll -Destination $Path/Mods -Force
+Move-Item -Path $Path/CinematicUnityExplorer.ML.IL2CPP.CoreCLR.pdb -Destination $Path/Mods -Force
 New-Item -Path "$Path" -Name "UserLibs" -ItemType "directory" -Force
 Move-Item -Path $Path/UniverseLib.ML.IL2CPP.Interop.dll -Destination $Path/UserLibs -Force
+Copy-Item UniverseLib/Release/UniverseLib.Il2Cpp.Interop/UniverseLib.ML.IL2CPP.Interop.pdb -Destination $Path/UserLibs -Force
 # (create zip archive)
 Remove-Item $Path/../CinematicUnityExplorer.MelonLoader.IL2CPP.CoreCLR.zip -ErrorAction SilentlyContinue
 compress-archive .\$Path\* $Path/../CinematicUnityExplorer.MelonLoader.IL2CPP.CoreCLR.zip
@@ -54,8 +58,10 @@ Remove-Item $Path/Iced.dll
 Remove-Item $Path/UnhollowerBaseLib.dll
 New-Item -Path "$Path" -Name "Mods" -ItemType "directory" -Force
 Move-Item -Path $Path/CinematicUnityExplorer.ML.IL2CPP.dll -Destination $Path/Mods -Force
+Move-Item -Path $Path/CinematicUnityExplorer.ML.IL2CPP.pdb -Destination $Path/Mods -Force
 New-Item -Path "$Path" -Name "UserLibs" -ItemType "directory" -Force
 Move-Item -Path $Path/UniverseLib.IL2CPP.Unhollower.dll -Destination $Path/UserLibs -Force
+Copy-Item UniverseLib/Release/UniverseLib.Il2Cpp.Unhollower/UniverseLib.IL2CPP.Unhollower.pdb -Destination $Path/UserLibs -Force
 # (create zip archive)
 Remove-Item $Path/../CinematicUnityExplorer.MelonLoader.IL2CPP.zip -ErrorAction SilentlyContinue
 compress-archive .\$Path\* $Path/../CinematicUnityExplorer.MelonLoader.IL2CPP.zip
@@ -70,8 +76,10 @@ Remove-Item $Path/Tomlet.dll
 Remove-Item $Path/mcs.dll
 New-Item -Path "$Path" -Name "Mods" -ItemType "directory" -Force
 Move-Item -Path $Path/CinematicUnityExplorer.ML.Mono.dll -Destination $Path/Mods -Force
+Move-Item -Path $Path/CinematicUnityExplorer.ML.Mono.pdb -Destination $Path/Mods -Force
 New-Item -Path "$Path" -Name "UserLibs" -ItemType "directory" -Force
 Move-Item -Path $Path/UniverseLib.Mono.dll -Destination $Path/UserLibs -Force
+Copy-Item UniverseLib/Release/UniverseLib.Mono/UniverseLib.Mono.pdb -Destination $Path/UserLibs -Force
 # (create zip archive)
 Remove-Item $Path/../CinematicUnityExplorer.MelonLoader.Mono.zip -ErrorAction SilentlyContinue
 compress-archive .\$Path\* $Path/../CinematicUnityExplorer.MelonLoader.Mono.zip
@@ -89,7 +97,9 @@ Remove-Item $Path/UnhollowerBaseLib.dll
 New-Item -Path "$Path" -Name "plugins" -ItemType "directory" -Force
 New-Item -Path "$Path" -Name "plugins/CinematicUnityExplorer" -ItemType "directory" -Force
 Move-Item -Path $Path/CinematicUnityExplorer.BIE.IL2CPP.dll -Destination $Path/plugins/CinematicUnityExplorer -Force
+Move-Item -Path $Path/CinematicUnityExplorer.BIE.IL2CPP.pdb -Destination $Path/plugins/CinematicUnityExplorer -Force
 Move-Item -Path $Path/UniverseLib.IL2CPP.Unhollower.dll -Destination $Path/plugins/CinematicUnityExplorer -Force
+Copy-Item UniverseLib/Release/UniverseLib.Il2Cpp.Unhollower/UniverseLib.IL2CPP.Unhollower.pdb -Destination $Path/plugins/CinematicUnityExplorer -Force
 # (create zip archive)
 Remove-Item $Path/../CinematicUnityExplorer.BepInEx.IL2CPP.zip -ErrorAction SilentlyContinue
 compress-archive .\$Path\* $Path/../CinematicUnityExplorer.BepInEx.IL2CPP.zip
@@ -110,7 +120,9 @@ Remove-Item $Path/CinematicUnityExplorer.BIE.IL2CPP.CoreCLR.deps.json
 New-Item -Path "$Path" -Name "plugins" -ItemType "directory" -Force
 New-Item -Path "$Path" -Name "plugins/CinematicUnityExplorer" -ItemType "directory" -Force
 Move-Item -Path $Path/CinematicUnityExplorer.BIE.IL2CPP.CoreCLR.dll -Destination $Path/plugins/CinematicUnityExplorer -Force
+Move-Item -Path $Path/CinematicUnityExplorer.BIE.IL2CPP.CoreCLR.pdb -Destination $Path/plugins/CinematicUnityExplorer -Force
 Move-Item -Path $Path/UniverseLib.BIE.IL2CPP.Interop.dll -Destination $Path/plugins/CinematicUnityExplorer -Force
+Copy-Item UniverseLib/Release/UniverseLib.Il2Cpp.Interop/UniverseLib.BIE.IL2CPP.Interop.pdb -Destination $Path/plugins/CinematicUnityExplorer -Force
 # (create zip archive)
 Remove-Item $Path/../CinematicUnityExplorer.BepInEx.IL2CPP.CoreCLR.zip -ErrorAction SilentlyContinue
 compress-archive .\$Path\* $Path/../CinematicUnityExplorer.BepInEx.IL2CPP.CoreCLR.zip
@@ -131,7 +143,9 @@ Remove-Item $Path/CinematicUnityExplorer.BIE.Unity.IL2CPP.CoreCLR.deps.json
 New-Item -Path "$Path" -Name "plugins" -ItemType "directory" -Force
 New-Item -Path "$Path" -Name "plugins/CinematicUnityExplorer" -ItemType "directory" -Force
 Move-Item -Path $Path/CinematicUnityExplorer.BIE.Unity.IL2CPP.CoreCLR.dll -Destination $Path/plugins/CinematicUnityExplorer -Force
+Move-Item -Path $Path/CinematicUnityExplorer.BIE.Unity.IL2CPP.CoreCLR.pdb -Destination $Path/plugins/CinematicUnityExplorer -Force
 Move-Item -Path $Path/UniverseLib.BIE.IL2CPP.Interop.dll -Destination $Path/plugins/CinematicUnityExplorer -Force
+Copy-Item UniverseLib/Release/UniverseLib.Il2Cpp.Interop/UniverseLib.BIE.IL2CPP.Interop.pdb -Destination $Path/plugins/CinematicUnityExplorer -Force
 # (create zip archive)
 Remove-Item $Path/../CinematicUnityExplorer.BepInEx.Unity.IL2CPP.CoreCLR.zip -ErrorAction SilentlyContinue
 compress-archive .\$Path\* $Path/../CinematicUnityExplorer.BepInEx.Unity.IL2CPP.CoreCLR.zip
@@ -147,7 +161,9 @@ Remove-Item $Path/mcs.dll
 New-Item -Path "$Path" -Name "plugins" -ItemType "directory" -Force
 New-Item -Path "$Path" -Name "plugins/CinematicUnityExplorer" -ItemType "directory" -Force
 Move-Item -Path $Path/CinematicUnityExplorer.BIE5.Mono.dll -Destination $Path/plugins/CinematicUnityExplorer -Force
+Move-Item -Path $Path/CinematicUnityExplorer.BIE5.Mono.pdb -Destination $Path/plugins/CinematicUnityExplorer -Force
 Move-Item -Path $Path/UniverseLib.Mono.dll -Destination $Path/plugins/CinematicUnityExplorer -Force
+Copy-Item UniverseLib/Release/UniverseLib.Mono/UniverseLib.Mono.pdb -Destination $Path/plugins/CinematicUnityExplorer -Force
 # (create zip archive)
 Remove-Item $Path/../CinematicUnityExplorer.BepInEx5.Mono.zip -ErrorAction SilentlyContinue
 compress-archive .\$Path\* $Path/../CinematicUnityExplorer.BepInEx5.Mono.zip
@@ -163,7 +179,9 @@ Remove-Item $Path/mcs.dll
 New-Item -Path "$Path" -Name "plugins" -ItemType "directory" -Force
 New-Item -Path "$Path" -Name "plugins/CinematicUnityExplorer" -ItemType "directory" -Force
 Move-Item -Path $Path/CinematicUnityExplorer.BIE6.Mono.dll -Destination $Path/plugins/CinematicUnityExplorer -Force
+Move-Item -Path $Path/CinematicUnityExplorer.BIE6.Mono.pdb -Destination $Path/plugins/CinematicUnityExplorer -Force
 Move-Item -Path $Path/UniverseLib.Mono.dll -Destination $Path/plugins/CinematicUnityExplorer -Force
+Copy-Item UniverseLib/Release/UniverseLib.Mono/UniverseLib.Mono.pdb -Destination $Path/plugins/CinematicUnityExplorer -Force
 # (create zip archive)
 Remove-Item $Path/../CinematicUnityExplorer.BepInEx6.Mono.zip -ErrorAction SilentlyContinue
 compress-archive .\$Path\* $Path/../CinematicUnityExplorer.BepInEx6.Mono.zip
@@ -179,7 +197,9 @@ Remove-Item $Path/mcs.dll
 New-Item -Path "$Path" -Name "plugins" -ItemType "directory" -Force
 New-Item -Path "$Path" -Name "plugins/CinematicUnityExplorer" -ItemType "directory" -Force
 Move-Item -Path $Path/CinematicUnityExplorer.BIE6.Unity.Mono.dll -Destination $Path/plugins/CinematicUnityExplorer -Force
+Move-Item -Path $Path/CinematicUnityExplorer.BIE6.Unity.Mono.pdb -Destination $Path/plugins/CinematicUnityExplorer -Force
 Move-Item -Path $Path/UniverseLib.Mono.dll -Destination $Path/plugins/CinematicUnityExplorer -Force
+Copy-Item UniverseLib/Release/UniverseLib.Mono/UniverseLib.Mono.pdb -Destination $Path/plugins/CinematicUnityExplorer -Force
 # (create zip archive)
 Remove-Item $Path/../CinematicUnityExplorer.BepInEx6.Unity.Mono.zip -ErrorAction SilentlyContinue
 compress-archive .\$Path\* $Path/../CinematicUnityExplorer.BepInEx6.Unity.Mono.zip
@@ -192,6 +212,7 @@ lib/ILRepack.exe /target:library /lib:lib/net35 /lib:$Path /internalize /out:$Pa
 # (cleanup and move files)
 Remove-Item $Path/Tomlet.dll
 Remove-Item $Path/mcs.dll
+Copy-Item UniverseLib/Release/UniverseLib.Mono/UniverseLib.Mono.pdb -Destination $Path -Force
 Remove-Item $Path/../CinematicUnityExplorer.Standalone.Mono.zip -ErrorAction SilentlyContinue
 compress-archive .\$Path\* $Path/../CinematicUnityExplorer.Standalone.Mono.zip
 
@@ -205,6 +226,7 @@ Remove-Item $Path/Tomlet.dll
 Remove-Item $Path/mcs.dll
 Remove-Item $Path/Iced.dll
 Remove-Item $Path/UnhollowerBaseLib.dll
+Copy-Item UniverseLib/Release/UniverseLib.Il2Cpp.Unhollower/UniverseLib.IL2CPP.Unhollower.pdb -Destination $Path -Force
 Remove-Item $Path/../CinematicUnityExplorer.Standalone.IL2CPP.zip -ErrorAction SilentlyContinue
 compress-archive .\$Path\* $Path/../CinematicUnityExplorer.Standalone.IL2CPP.zip
 
@@ -212,6 +234,8 @@ compress-archive .\$Path\* $Path/../CinematicUnityExplorer.Standalone.IL2CPP.zip
 $Path1 = "Release/CinematicUnityExplorer.Standalone.Mono"
 $Path2 = "UnityEditorPackage/Runtime"
 Copy-Item $Path1/CinematicUnityExplorer.STANDALONE.Mono.dll -Destination $Path2
+Copy-Item $Path1/CinematicUnityExplorer.STANDALONE.Mono.pdb -Destination $Path2
 Copy-Item $Path1/UniverseLib.Mono.dll -Destination $Path2
+Copy-Item $Path1/UniverseLib.Mono.pdb -Destination $Path2
 Remove-Item Release/CinematicUnityExplorer.Editor.zip -ErrorAction SilentlyContinue
 compress-archive .\UnityEditorPackage\*  Release/CinematicUnityExplorer.Editor.zip
